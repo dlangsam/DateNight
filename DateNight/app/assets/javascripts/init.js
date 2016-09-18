6e429780b0a88bb3df602ac3189bb8b3400dc801
Devorah Langsam <devorah@langsam.org>
2016-09-18 08:39:15 -0400

    connected js

---
+++ DateNight/app/assets/javascripts/init.js
@@ -0,0 +1,38 @@
+if (window.DateNightApp === undefined){
+	window.DateNightApp = {};
+}
+DateNightApp.init = function(){
+	console.log("DateNightApp ONLINE");
+};
+
+$(document).on("turbolinks:load", function(){
+
+	if("geolocation" in navigator){
+		console.log("Broswer has geolocation");
+		var options = {timeout: 10000000};
+		navigator.geolocation.getCurrentPosition(function(position){}, handleError, options);
+	}else{
+		var html = `
+		<h2>Update your browser. </h2>
+		`;
+		$("body").append(html);
+		console.log("Browser doesn't have geolation");
+	}
+	DateNightApp.init();
+	//if (/^\/categories/.test(window.location.pathname) { // do stuff here }`
+	//FIXME!!!!!!
+	
+
+	function handleError(error){
+		console.log("There is an error.");
+		console.log(error);
+	}
+
+	window.onresize = function() {
+    if (window.innerHeight >= 820) { /* ... */ }
+    if (window.innerWidth <= 1130) {  
+    	// $('.navbar-collapse').removeClass("collapse");
+    	// $('.navbar-collapse').addClass("collapsed");
+    /* ... */ }
+}
+});
