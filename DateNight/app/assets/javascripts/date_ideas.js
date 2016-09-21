3d0051d14ebb544891e53833471b575f52b89ab0
Devorah Langsam <devorah@langsam.org>
2016-09-20 22:11:50 -0400

    activate button and add yelp gem

--- DateNight/app/assets/javascripts/date_ideas.js
+++ DateNight/app/assets/javascripts/date_ideas.js
@@ -1,5 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).on("turbolinks:load", function(){
+	$('.js-search').on('click', function(e){
+		e.preventDefault();
+		console.log("Button clicked");
+	});
 
-}
+
+})
--- DateNight/config/routes.rb
+++ DateNight/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "date_ideas#index"
   resources :date_ideas
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
